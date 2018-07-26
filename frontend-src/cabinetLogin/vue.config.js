91f586cbbf9e3b64eb0eec5759c2411f45125b79
Vladislav Slizhevsky <VladislavSlizhevsky@coherentsolutions.com>
2018-07-26 10:43:30 +0300

    some changes in vue cli config file

--- frontend-src/cabinetLogin/vue.config.js
+++ frontend-src/cabinetLogin/vue.config.js
@@ -1,15 +1,3 @@
 module.exports = {
-	// css: {
-	//     extract: {
-	//         filename: 'cabinetLogin/css/[name].css',
-	//         chunkFilename: 'css/[name].css',
-	//     },
-	// },
-	// configureWebpack: {
-	//     output: {
-	//         filename: 'cabinetLogin/js/[name].js',
-	//         chunkFilename: 'js/[name].js',
-	//     }
-	// }
 	baseUrl: "/cabinetLogin/"
 };
