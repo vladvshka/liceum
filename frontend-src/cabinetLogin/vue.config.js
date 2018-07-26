f5a623127e9719b087eb70863efb22a79874e9c8
Vladislav Slizhevsky <VladislavSlizhevsky@coherentsolutions.com>
2018-07-26 10:22:01 +0300

    added vue-cli config

---
+++ frontend-src/cabinetLogin/vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	// css: {
+	//     extract: {
+	//         filename: 'cabinetLogin/css/[name].css',
+	//         chunkFilename: 'css/[name].css',
+	//     },
+	// },
+	// configureWebpack: {
+	//     output: {
+	//         filename: 'cabinetLogin/js/[name].js',
+	//         chunkFilename: 'js/[name].js',
+	//     }
+	// }
+	baseUrl: "/cabinetLogin/"
+};
--- routes/cabinet.js
+++ routes/cabinet.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const path = require("path");
 
 router.get('/', function (req, res, next) {
-    res.sendFile(path.join(__dirname, '../public/cabinet/index.html'));
+    res.sendFile(path.join(__dirname, '../public/cabinetLogin/index.html'));
+
 });
 
 module.exports = router;
