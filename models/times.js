22fdf6fca8a29d5a7865eee5bc5b15ce15921819
Yury Imbro <imbroyury@gmail.com>
2018-07-31 19:20:21 +0300

    Merge branch 'master' into admin-frontend-setup
    weird conflict resolution №2

--- models/times.js
+++ models/times.js
@@ -13,18 +13,39 @@ const schema = new Schema({
     "required": true
   },
   "updated": {
-    "type": Date
+    "type": Date,
+		"default": Date.now
   },
   "created": {
     "type": Date,
     "default": Date.now
   }
 });
-schema.pre('save', beforeSave);
-const model = mongoose.model('times', schema);
-async function beforeSave() {
-  console.log('***before save fired***');
-  console.log(this);
-  this.updated = await Date.now();
+
+schema.pre('update', updateDate);
+
+function updateDate() {
+	console.log('updating');
+	
+	this.update({}, {
+		$set: {
+			updated: new Date()
+		}
+	});
 }
+
+schema.statics.findAndUpdateById = function (id, update, res) {
+	this.update({
+			_id: id
+		}, update)
+		.then(function (doc) {
+			res.sendStatus(200);
+		})
+		.catch(function (err) {
+			console.error(err);
+			res.status(500).send("DB updating error");
+		});
+}
+
+const model = mongoose.model('times', schema);
 module.exports = model;
